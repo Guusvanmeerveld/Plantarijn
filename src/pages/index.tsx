d779a5f7693b411b9ccd0c4729b4be41d2eb16a7
Netfloex <netfloex@gmail.com>
2021-06-22 20:56:31 +0200

    GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,29 @@ import { NextSeo } from "next-seo";
 
 import Title from "@components/Title";
 
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+import createDatabase from "utils/createDatabase";
+import Schema from "models/database";
 
-const Index: NextPage = () => (
+type Props = {
+	data: Schema;
+};
+
+const Index: NextPage<Props> = ({ data }) => (
 	<>
 		<NextSeo title="Hello World" />
 		<Title />
+		{JSON.stringify(data)}
 	</>
 );
+export const getStaticProps: GetStaticProps<Props> = async () => {
+	const store = createDatabase();
+	return {
+		props: {
+			data: store.data
+		},
+		revalidate: 10
+	};
+};
 
 export default Index;
